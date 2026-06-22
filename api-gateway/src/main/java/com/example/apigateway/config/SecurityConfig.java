de91901c387559bd3ba7734b108418866e6c6007
Jonathan Isaksson <tignaltus@gmail.com>
2026-06-22 22:28:36 +0200

    Uppdatering till Authentikation
    Använder oss av rollerna ADMIN och USER som vi redan har för att tillåta vilka tjänster de kan komma åt.

--- api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
@@ -41,15 +41,30 @@ public class SecurityConfig {
                         .pathMatchers("/auth/login").permitAll()
                         .pathMatchers("/actuator/health").permitAll()
                         .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                        .anyExchange().authenticated()
-                )
+
+                        // Authors
+                        .pathMatchers(HttpMethod.GET, "/api/v1/authors/**").hasAnyRole("USER", "ADMIN")
+                        .pathMatchers(HttpMethod.POST, "/api/v1/authors/**").hasRole("ADMIN")
+                        .pathMatchers(HttpMethod.PUT, "/api/v1/authors/**").hasRole("ADMIN")
+                        .pathMatchers(HttpMethod.DELETE, "/api/v1/authors/**").hasRole("ADMIN")
+
+                        // Loans
+                        .pathMatchers(HttpMethod.GET, "/api/v1/loans/**").hasAnyRole("USER", "ADMIN")
+                        .pathMatchers(HttpMethod.POST, "/api/v1/loans/**").hasAnyRole("USER", "ADMIN")
+                        .pathMatchers(HttpMethod.PUT, "/api/v1/loans/**").hasRole("ADMIN")
+                        .pathMatchers(HttpMethod.DELETE, "/api/v1/loans/**").hasRole("ADMIN")
+
+                        // Books
+                        .pathMatchers(HttpMethod.GET, "/api/v1/books/**").hasAnyRole("USER", "ADMIN")
+                        .pathMatchers(HttpMethod.POST, "/api/v1/books/**").hasRole("ADMIN")
+                        .pathMatchers(HttpMethod.PUT, "/api/v1/books/**").hasRole("ADMIN")
+                        .pathMatchers(HttpMethod.DELETE, "/api/v1/books/**").hasRole("ADMIN")
+
+                        .anyExchange().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
                                 .jwtDecoder(jwtDecoder)
-                                .jwtAuthenticationConverter(jwtAuthenticationConverter)
-                        )
-                )
-                .build();
+                                .jwtAuthenticationConverter(jwtAuthenticationConverter))).build();
     }
 
     @Bean
