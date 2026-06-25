c5efd17d56790ed7e2b83f5323982e54b35d7ae1
kostas900402 <nascoskostas@gmail.com>
2026-06-25 08:53:23 +0200

    test2

--- author-microservice/src/main/java/com/assignment/author_microservice/client/BookClient.java
+++ author-microservice/src/main/java/com/assignment/author_microservice/client/BookClient.java
@@ -3,12 +3,13 @@ package com.assignment.author_microservice.client;
 import com.assignment.author_microservice.dto.BookResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "book-service")
+@FeignClient(name = "book-microservice")
 public interface BookClient {
 
-    @GetMapping("/api/v1/books")
-    List<BookResponse> getAllBooks();
+    @GetMapping("/api/v1/books/author/{authorId}")
+    List<BookResponse> getBooksByAuthor(@PathVariable Long authorId);
 }
