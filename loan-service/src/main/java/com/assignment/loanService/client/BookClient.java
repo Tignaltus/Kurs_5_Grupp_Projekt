3a020532e196a114ca1cf37e241abd595b2a8d9a
felix <felix@lidenmail.se>
2026-06-25 02:40:09 +0200

    fixat eureka för bookservice och loanservice samt lagt till api-gateway och book service som moduler i pom.xml i huvudmappen

--- loan-service/src/main/java/com/assignment/loanService/client/BookClient.java
+++ loan-service/src/main/java/com/assignment/loanService/client/BookClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "book-service", url = "${services.book.url}")
+@FeignClient(name = "book-service")
 public interface BookClient {
 
     @GetMapping("/api/v1/books/{id}")
